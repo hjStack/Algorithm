1897c58983a6a7e89b5199b51516f1cb8d16572f
HJ <165659365+hjStack@users.noreply.github.com>
2025-07-07 14:49:13 +0900

    [Bronze III] Title: 약수 구하기, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

--- Java/백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ Java/백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,7 +1,7 @@
 import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
 
        Scanner scanner=new Scanner(System.in);
@@ -10,7 +10,10 @@ public class Main{
        int k=scanner.nextInt();
        int count=0;
 
-       //  N의 약수들 중 K번째로 작은 수를 출력
+        // N의 약수들 중 K번째로 작은 수를 출력
+        // 약수를 찾으면 카운트를 하고
+        // 카운트 값과 k가 같으면 약수를 그대로 출력해줌
+
        for(int i=1; i<=n; i++){
            if(n % i == 0){
                count+=1;
